6e0cae30132d9b898867d48ced7530d34a5dd0f2
Ruben Baskaran <ruba@mmmi.sdu.dk>
2020-08-10 16:58:07 +0200

    removed redundant iomports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Introduction from "./components/introduction";
 import HumanCheck from "./components/humanCheck";
 import FirstSurvey from "./components/firstSurvey";
