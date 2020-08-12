6918ee46fc8b5460eb9706d89e9c8a547bbeb980
Ruben Baskaran <ruba@mmmi.sdu.dk>
2020-08-12 17:01:04 +0200

    added styling to material ui and changed class component to function

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -1,26 +1,54 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
+import { Container, Button, Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-class Introduction extends Component {
-  render() {
-    return (
-      <div>
-        This is the introduction page
-        <br />
-        <a href="https://alkohologsamfund.dk/">
-          <Button variant="contained" color="primary">
-            Back to alkohologsamfund
-          </Button>
-        </a>
-        <Link to="/humancheck">
-          <Button variant="contained" color="primary">
-            Go to human check
-          </Button>
-        </Link>
-      </div>
-    );
-  }
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+function Introduction() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="sm">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <h1>This is the introduction page</h1>
+          </Grid>
+          <Grid item xs={6}>
+            <a href="https://alkohologsamfund.dk/">
+              <Button variant="contained" color="primary">
+                Back to alkohologsamfund
+              </Button>
+            </a>
+          </Grid>
+          <Grid item xs={6}>
+            <Link to="/humancheck">
+              <Button variant="contained" color="primary">
+                Go to human check
+              </Button>
+            </Link>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>xs=3</Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default Introduction;
