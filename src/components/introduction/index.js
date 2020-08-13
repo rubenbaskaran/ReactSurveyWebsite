54d95e52c1c67c53f75477d8227c69d3937f78b1
Ruben Baskaran <ruba@mmmi.sdu.dk>
2020-08-13 15:45:40 +0200

    added rating scale

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -1,21 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Button, Grid, Paper } from "@material-ui/core";
+import {
+  Container,
+  Button,
+  Grid,
+  Paper,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
+  paper: {
+    padding: 20,
   },
 });
 
 function Introduction() {
+  const [rating, setRating] = React.useState(5);
+  const [spacing] = React.useState(1);
   const classes = useStyles();
 
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setRating(Number(event.target.value));
+  };
+
   return (
-    <div className={classes.root}>
-      <Container maxWidth="sm">
-        <Grid container spacing={1}>
+    <div>
+      <Container fixed>
+        <Grid container spacing={spacing}>
           <Grid item xs={12}>
             <h1>This is the introduction page</h1>
           </Grid>
@@ -36,14 +52,35 @@ function Introduction() {
           <Grid item xs={3}>
             <Paper>xs=3</Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper>xs=3</Paper>
+          <Grid item xs={6}>
+            <Paper>xs=6</Paper>
           </Grid>
           <Grid item xs={3}>
             <Paper>xs=3</Paper>
           </Grid>
-          <Grid item xs={3}>
-            <Paper>xs=3</Paper>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Grid container>
+                <Grid item>
+                  <FormLabel>Question 1: Write the question here...</FormLabel>
+                  <RadioGroup
+                    name="questionOne"
+                    value={rating.toString()}
+                    onChange={handleChange}
+                    row
+                  >
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+                      <FormControlLabel
+                        key={value}
+                        value={value.toString()}
+                        control={<Radio />}
+                        label={value.toString()}
+                      />
+                    ))}
+                  </RadioGroup>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
         </Grid>
       </Container>
