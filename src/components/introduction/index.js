4e59ad1d823c4245bb0b398dd0b56c00105b14f3
Ruben Baskaran <ruba@mmmi.sdu.dk>
2020-08-13 16:30:32 +0200

    added styling and removed redundant grid tags

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -13,14 +13,21 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  paper: {
+  grid: {
+    background: "teal",
+  },
+  ratingScale: {
     padding: 20,
+  },
+  question: {
+    color: "black",
   },
 });
 
 function Introduction() {
   const [rating, setRating] = React.useState(5);
   const [spacing] = React.useState(1);
+
   const classes = useStyles();
 
   const handleChange = (event) => {
@@ -31,7 +38,7 @@ function Introduction() {
   return (
     <div>
       <Container fixed>
-        <Grid container spacing={spacing}>
+        <Grid container spacing={spacing} className={classes.grid}>
           <Grid item xs={12}>
             <h1>This is the introduction page</h1>
           </Grid>
@@ -59,27 +66,25 @@ function Introduction() {
             <Paper>xs=3</Paper>
           </Grid>
           <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Grid container>
-                <Grid item>
-                  <FormLabel>Question 1: Write the question here...</FormLabel>
-                  <RadioGroup
-                    name="questionOne"
-                    value={rating.toString()}
-                    onChange={handleChange}
-                    row
-                  >
-                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-                      <FormControlLabel
-                        key={value}
-                        value={value.toString()}
-                        control={<Radio />}
-                        label={value.toString()}
-                      />
-                    ))}
-                  </RadioGroup>
-                </Grid>
-              </Grid>
+            <Paper className={classes.ratingScale}>
+              <FormLabel className={classes.question}>
+                Question 1: Write the question here...
+              </FormLabel>
+              <RadioGroup
+                name="questionOne"
+                value={rating.toString()}
+                onChange={handleChange}
+                row
+              >
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+                  <FormControlLabel
+                    key={value}
+                    value={value.toString()}
+                    control={<Radio />}
+                    label={value.toString()}
+                  />
+                ))}
+              </RadioGroup>
             </Paper>
           </Grid>
         </Grid>
