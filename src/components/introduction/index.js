062bca1d0da4d5e6153446ad2a8a78129ed3d65b
Ruben Baskaran <ruba@mmmi.sdu.dk>
2020-08-14 13:34:48 +0200

    renamed folder for ratingScale

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Button, Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import RatingScale from "../questionTypes";
+import RatingScale from "../ratingScale";
 
 const useStyles = makeStyles({
   grid: {
