8152a39b2cd49aba64b6fc321e9cf6bd8cbcd4e3
Ruben Baskaran <ruba@mmmi.sdu.dk>
2020-08-14 15:27:30 +0200

    did a lot of fine adjustments

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -4,21 +4,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import RatingScale from "../ratingScale";
 
 const useStyles = makeStyles({
+  container: {
+    "margin-top": "20px",
+  },
   grid: {
-    background: "teal",
+    background: "#f3f3f3",
+    padding: "20px",
   },
   childGrid: {
-    background: "pink",
-  },
-  ratingScale: {
-    padding: 20,
-  },
-  question: {
-    color: "black",
+    background: "#f3f3f3",
   },
   button: {
-    color: "yellow",
     width: "100%",
+    "margin-top": "15px",
   },
 });
 
@@ -28,10 +26,31 @@ function Introduction() {
 
   return (
     <div>
-      <Container fixed>
+      <Container fixed className={classes.container}>
         <Grid container spacing={spacing} className={classes.grid}>
           <Grid item xs={12}>
             <h1>This is the introduction page</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu
+              nunc id leo tristique blandit. Integer at dolor ornare, blandit
+              nunc tristique, volutpat tortor. Phasellus quis magna posuere,
+              faucibus lectus sit amet, venenatis nibh. Proin efficitur
+              dignissim accumsan. Maecenas magna sem, suscipit nec odio ac,
+              accumsan blandit felis. Suspendisse tincidunt massa sit amet
+              consequat mattis. Curabitur interdum faucibus justo, et
+              ullamcorper odio euismod ut. Nulla pellentesque feugiat nunc, ut
+              luctus urna elementum a. Proin in cursus mauris. Donec tempor
+              porttitor porttitor. Quisque at erat auctor, malesuada velit
+              eleifend, malesuada urna. Donec in elit sit amet tellus consequat
+              placerat. Mauris sit amet tempor elit. Mauris libero turpis,
+              laoreet ac pretium non, condimentum vel ex.
+            </p>
+          </Grid>
+          <Grid item xs={12}>
+            <RatingScale question="Question 1: Write the question here..." />
+          </Grid>
+          <Grid item xs={12}>
+            <RatingScale question="Question 2: Write the question here..." />
           </Grid>
           <Grid container item sm={6} xs={12} className={classes.childGrid}>
             <Button
@@ -52,21 +71,6 @@ function Introduction() {
             >
               Go to human check
             </Button>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper>xs=3</Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper>xs=6</Paper>
-          </Grid>
-          <Grid item xs={3}>
-            <Paper>xs=3</Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <RatingScale question="Question 1: Write the question here..." />
-          </Grid>
-          <Grid item xs={12}>
-            <RatingScale question="Question 2: Write the question here..." />
           </Grid>
         </Grid>
       </Container>
--- src/components/ratingScale/index.js
+++ src/components/ratingScale/index.js
@@ -10,11 +10,34 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  ratingScale: {
+  paper: {
     padding: 20,
   },
   question: {
     color: "black",
+    background: "#bccfe8",
+    display: "block",
+    width: "100%",
+    padding: "15px",
+  },
+  ratingScale: {
+    "margin-left": "auto",
+    "margin-right": "auto",
+    "margin-top": "20px",
+    "background-color": "pink",
+    display: "inline-block",
+  },
+  required: {
+    display: "block",
+    "text-align": "right",
+    color: "red",
+    "padding-top": "15px",
+  },
+  goodBadText: {
+    display: "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "margin-top": "20px",
   },
 });
 
@@ -28,23 +51,30 @@ function RatingScale(props) {
   };
 
   return (
-    <Paper className={classes.ratingScale}>
-      <FormLabel className={classes.question}>{props.question}</FormLabel>
+    <Paper className={classes.paper}>
       <RadioGroup
         name="questionOne"
         value={rating.toString()}
         onChange={handleChange}
         row
       >
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-          <FormControlLabel
-            key={value}
-            value={value.toString()}
-            control={<Radio />}
-            label={value.toString()}
-          />
-        ))}
+        <FormLabel className={classes.question}>{props.question}</FormLabel>
+        <div className={classes.goodBadText}>Very bad</div>
+        <div className={classes.ratingScale}>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+            <FormControlLabel
+              key={value}
+              value={value.toString()}
+              control={<Radio />}
+              label={value.toString()}
+            />
+          ))}
+        </div>
+        <div className={classes.goodBadText}>Very good</div>
       </RadioGroup>
+      <div className={classes.required}>
+        <label>*required</label>
+      </div>
     </Paper>
   );
 }
