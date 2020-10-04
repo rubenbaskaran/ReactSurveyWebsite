f63b1bc3c73784a3df0260aa06e6f3f77fffb9bf
Ruben Baskaran <rbaskaran92@gmail.com>
2020-10-04 14:24:30 +0200

    added randomization of videos

--- src/components/pages/randomVideo/index.js
+++ src/components/pages/randomVideo/index.js
@@ -4,11 +4,27 @@ import { Container, Button, Grid } from "@material-ui/core";
 import useStyles from "../../styles";
 import LinearWithValueLabel from "../../progressBar";
 
-// TODO: Add randomization
-
 function RandomVideo() {
   const [spacing] = React.useState(1);
   const classes = useStyles();
+
+  let url = "";
+  const urlConfigs = "?autoplay=1&modestbranding=1&fs=0&rel=0&cc_load_policy=1";
+
+  switch (Math.floor(Math.random() * 3) + 1) {
+    case 1:
+      url = "https://www.youtube.com/embed/ElIl2gXQUnw" + urlConfigs;
+      console.log("video 1");
+      break;
+    case 2:
+      url = "https://www.youtube.com/embed/A3kmaB_r_e0" + urlConfigs;
+      console.log("video 2");
+      break;
+    case 3:
+      url = "https://www.youtube.com/embed/HRCP0xfc7b8" + urlConfigs;
+      console.log("video 3");
+      break;
+  }
 
   return (
     <div>
@@ -19,7 +35,7 @@ function RandomVideo() {
             <iframe
               width="760"
               height="515"
-              src="https://www.youtube.com/embed/ElIl2gXQUnw?autoplay=1&modestbranding=1&fs=0&rel=0&cc_load_policy=1"
+              src={url}
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             ></iframe>
