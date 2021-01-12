66778f036f8c8ec13fbbb4312d33db977c0f2556
Ruben Baskaran <rbaskaran92@gmail.com>
2021-01-12 18:12:41 +0100

    updated content of calls to web api

--- src/components/CallsToBackend.js
+++ src/components/CallsToBackend.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// TODO: Create/update redcap record from here
-function CallNodeBackend() {
+// TODO: Insert record id in all calls besides the first one
+function UploadFirst1To7() {
   let url = "http://localhost:3001/create";
   let data = {
     record_id: "null",
@@ -12,6 +12,83 @@ function CallNodeBackend() {
     question_5: "question_5",
     question_6: "question_6",
     question_7: "question_7",
+    question_8: "",
+    question_9: "",
+    question_10: "",
+    question_11: "",
+    question_12: "",
+    question_13: "",
+    question_14: "",
+    question_15: "",
+    question_16: "",
+    question_17: "",
+    question_18: "",
+    question_19: "",
+    question_20: "",
+    question_21: "",
+    question_22: "",
+    question_23: "",
+    question_24: "",
+    question_25: "",
+    question_26: "",
+    question_27: "",
+    question_28: "",
+    question_29: "",
+    question_30: "",
+    question_31: "",
+    question_32: "",
+    question_33: "",
+    question_34: "",
+    question_35: "",
+    question_36: "",
+    question_37: "",
+    question_38: "",
+    question_39: "",
+    question_40: "",
+    question_41: "",
+    question_42: "",
+    question_43: "",
+    question_44: "",
+    question_45: "",
+    question_46: "",
+    question_47: "",
+    question_48: "",
+    question_49: "",
+    question_50: "",
+    question_51: "",
+    question_52: "",
+    question_53: "",
+    question_54: "",
+    question_55: "",
+    question_56: "",
+    question_57: "",
+    question_58: "",
+    answers_complete: 0,
+  };
+
+  axios
+    .post(url, data)
+    .then(function (response) {
+      console.log("**RESPONSE**");
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("**ERROR**");
+      console.log(error);
+    });
+}
+
+function UploadSecond8To26() {
+  let url = "http://localhost:3001/update";
+  let data = {
+    record_id: "NUMBER!",
+    question_1: "",
+    question_2: "",
+    question_3: "",
+    question_4: "",
+    question_5: "",
+    question_6: "",
+    question_7: "",
     question_8: "question_8",
     question_9: "question_9",
     question_10: "question_10",
@@ -31,6 +108,83 @@ function CallNodeBackend() {
     question_24: "question_24",
     question_25: "question_25",
     question_26: "question_26",
+    question_27: "",
+    question_28: "",
+    question_29: "",
+    question_30: "",
+    question_31: "",
+    question_32: "",
+    question_33: "",
+    question_34: "",
+    question_35: "",
+    question_36: "",
+    question_37: "",
+    question_38: "",
+    question_39: "",
+    question_40: "",
+    question_41: "",
+    question_42: "",
+    question_43: "",
+    question_44: "",
+    question_45: "",
+    question_46: "",
+    question_47: "",
+    question_48: "",
+    question_49: "",
+    question_50: "",
+    question_51: "",
+    question_52: "",
+    question_53: "",
+    question_54: "",
+    question_55: "",
+    question_56: "",
+    question_57: "",
+    question_58: "",
+    answers_complete: 0,
+  };
+
+  axios
+    .post(url, data)
+    .then(function (response) {
+      console.log("**RESPONSE**");
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("**ERROR**");
+      console.log(error);
+    });
+}
+
+function UploadThird27To34() {
+  let url = "http://localhost:3001/update";
+  let data = {
+    record_id: "NUMBER!",
+    question_1: "",
+    question_2: "",
+    question_3: "",
+    question_4: "",
+    question_5: "",
+    question_6: "",
+    question_7: "",
+    question_8: "",
+    question_9: "",
+    question_10: "",
+    question_11: "",
+    question_12: "",
+    question_13: "",
+    question_14: "",
+    question_15: "",
+    question_16: "",
+    question_17: "",
+    question_18: "",
+    question_19: "",
+    question_20: "",
+    question_21: "",
+    question_22: "",
+    question_23: "",
+    question_24: "",
+    question_25: "",
+    question_26: "",
     question_27: "question_27",
     question_28: "question_28",
     question_29: "question_29",
@@ -39,6 +193,83 @@ function CallNodeBackend() {
     question_32: "question_32",
     question_33: "question_33",
     question_34: "question_34",
+    question_35: "",
+    question_36: "",
+    question_37: "",
+    question_38: "",
+    question_39: "",
+    question_40: "",
+    question_41: "",
+    question_42: "",
+    question_43: "",
+    question_44: "",
+    question_45: "",
+    question_46: "",
+    question_47: "",
+    question_48: "",
+    question_49: "",
+    question_50: "",
+    question_51: "",
+    question_52: "",
+    question_53: "",
+    question_54: "",
+    question_55: "",
+    question_56: "",
+    question_57: "",
+    question_58: "",
+    answers_complete: 0,
+  };
+
+  axios
+    .post(url, data)
+    .then(function (response) {
+      console.log("**RESPONSE**");
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("**ERROR**");
+      console.log(error);
+    });
+}
+
+function UploadFourth35To49() {
+  let url = "http://localhost:3001/update";
+  let data = {
+    record_id: "NUMBER!",
+    question_1: "",
+    question_2: "",
+    question_3: "",
+    question_4: "",
+    question_5: "",
+    question_6: "",
+    question_7: "",
+    question_8: "",
+    question_9: "",
+    question_10: "",
+    question_11: "",
+    question_12: "",
+    question_13: "",
+    question_14: "",
+    question_15: "",
+    question_16: "",
+    question_17: "",
+    question_18: "",
+    question_19: "",
+    question_20: "",
+    question_21: "",
+    question_22: "",
+    question_23: "",
+    question_24: "",
+    question_25: "",
+    question_26: "",
+    question_27: "",
+    question_28: "",
+    question_29: "",
+    question_30: "",
+    question_31: "",
+    question_32: "",
+    question_33: "",
+    question_34: "",
     question_35: "question_35",
     question_36: "question_36",
     question_37: "question_37",
@@ -54,15 +285,16 @@ function CallNodeBackend() {
     question_47: "question_47",
     question_48: "question_48",
     question_49: "question_49",
-    question_50: "question_50",
-    question_51: "question_51",
-    question_52: "question_52",
-    question_53: "question_53",
-    question_54: "question_54",
-    question_55: "question_55",
-    question_56: "question_56",
-    question_57: "question_57",
-    question_58: "question_58",
+    question_50: "",
+    question_51: "",
+    question_52: "",
+    question_53: "",
+    question_54: "",
+    question_55: "",
+    question_56: "",
+    question_57: "",
+    question_58: "",
+    answers_complete: 0,
   };
 
   axios
@@ -77,8 +309,165 @@ function CallNodeBackend() {
     });
 }
 
-function Test() {
-  console.log("this is a test");
+function UploadFifth50To57() {
+  let url = "http://localhost:3001/update";
+  let data = {
+    record_id: "NUMBER",
+    question_1: "",
+    question_2: "",
+    question_3: "",
+    question_4: "",
+    question_5: "",
+    question_6: "",
+    question_7: "",
+    question_8: "",
+    question_9: "",
+    question_10: "",
+    question_11: "",
+    question_12: "",
+    question_13: "",
+    question_14: "",
+    question_15: "",
+    question_16: "",
+    question_17: "",
+    question_18: "",
+    question_19: "",
+    question_20: "",
+    question_21: "",
+    question_22: "",
+    question_23: "",
+    question_24: "",
+    question_25: "",
+    question_26: "",
+    question_27: "",
+    question_28: "",
+    question_29: "",
+    question_30: "",
+    question_31: "",
+    question_32: "",
+    question_33: "",
+    question_34: "",
+    question_35: "",
+    question_36: "",
+    question_37: "",
+    question_38: "",
+    question_39: "",
+    question_40: "",
+    question_41: "",
+    question_42: "",
+    question_43: "",
+    question_44: "",
+    question_45: "",
+    question_46: "",
+    question_47: "",
+    question_48: "",
+    question_49: "",
+    question_50: "question_50",
+    question_51: "question_51",
+    question_52: "question_52",
+    question_53: "question_53",
+    question_54: "question_54",
+    question_55: "question_55",
+    question_56: "question_56",
+    question_57: "question_57",
+    question_58: "",
+    answers_complete: 0,
+  };
+
+  axios
+    .post(url, data)
+    .then(function (response) {
+      console.log("**RESPONSE**");
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("**ERROR**");
+      console.log(error);
+    });
 }
 
-export { CallNodeBackend, Test };
+function UploadSixth58() {
+  let url = "http://localhost:3001/update";
+  let data = {
+    record_id: "NUMBER",
+    question_1: "",
+    question_2: "",
+    question_3: "",
+    question_4: "",
+    question_5: "",
+    question_6: "",
+    question_7: "",
+    question_8: "",
+    question_9: "",
+    question_10: "",
+    question_11: "",
+    question_12: "",
+    question_13: "",
+    question_14: "",
+    question_15: "",
+    question_16: "",
+    question_17: "",
+    question_18: "",
+    question_19: "",
+    question_20: "",
+    question_21: "",
+    question_22: "",
+    question_23: "",
+    question_24: "",
+    question_25: "",
+    question_26: "",
+    question_27: "",
+    question_28: "",
+    question_29: "",
+    question_30: "",
+    question_31: "",
+    question_32: "",
+    question_33: "",
+    question_34: "",
+    question_35: "",
+    question_36: "",
+    question_37: "",
+    question_38: "",
+    question_39: "",
+    question_40: "",
+    question_41: "",
+    question_42: "",
+    question_43: "",
+    question_44: "",
+    question_45: "",
+    question_46: "",
+    question_47: "",
+    question_48: "",
+    question_49: "",
+    question_50: "",
+    question_51: "",
+    question_52: "",
+    question_53: "",
+    question_54: "",
+    question_55: "",
+    question_56: "",
+    question_57: "",
+    question_58: "question_58",
+    answers_complete: 2,
+  };
+
+  axios
+    .post(url, data)
+    .then(function (response) {
+      console.log("**RESPONSE**");
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("**ERROR**");
+      console.log(error);
+    });
+}
+
+export {
+  UploadFirst1To7,
+  UploadSecond8To26,
+  UploadThird27To34,
+  UploadFourth35To49,
+  UploadFifth50To57,
+  UploadSixth58,
+};
