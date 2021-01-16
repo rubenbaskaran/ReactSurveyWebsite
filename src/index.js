78497c24742185ed28a0a296cec1a91324d5033d
Ruben Baskaran <rbaskaran92@gmail.com>
2021-01-16 03:24:49 +0100

    refactored index file for root page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { HashRouter, Route, Switch } from "react-router-dom";
 import Consent from "./components/pages/consent";
 import PersonalInfo from "./components/pages/personalInfo";
 import FirstSurvey from "./components/pages/firstSurvey";
@@ -15,18 +14,6 @@ import CompetitionParticipation from "./components/pages/competitionParticipatio
 import "fontsource-roboto";
 import store from "./globalVariables";
 import { Provider } from "react-redux";
-
-// TODO: Replace exact paths with following codes
-// dthCTmsuY0
-// LVZqe449a3
-// jip0n9h2EG
-// cKyqRYVsOE
-// 6nd7uhgvXp
-// KJ8sL0ZGGp
-// SJtaoT6Epc
-// Np6xtbeNHx
-// tt2KrCfgA3
-// 6BAD8j5Vdv
 
 const App = () => {
   const [currentPage, setCurrentPage] = React.useState(0);
@@ -61,40 +48,8 @@ const App = () => {
         ) : null}
       </div>
     </Provider>
-
-    // <HashRouter>
-    //   <Switch>
-    //     <Provider store={store}>
-    //       <Route exact path="/" component={Consent} />
-    //       <Route exact path="/personalinfo" component={PersonalInfo} />
-    //       <Route exact path="/firstsurvey" component={FirstSurvey} />
-    //       <Route
-    //         exact
-    //         path="/preninetydaythoughts"
-    //         component={PreNinetyDayThoughts}
-    //       />
-    //       <Route exact path="/videotext" component={VideoText} />
-    //       <Route exact path="/randomvideo" component={RandomVideo} />
-    //       <Route exact path="/secondsurvey" component={SecondSurvey} />
-    //       <Route
-    //         exact
-    //         path="/postninetydaythoughts"
-    //         component={PostNinetyDayThoughts}
-    //       />
-    //       <Route
-    //         exact
-    //         path="/competitionparticipation"
-    //         component={CompetitionParticipation}
-    //       />
-    //     </Provider>
-    //   </Switch>
-    // </HashRouter>
   );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
