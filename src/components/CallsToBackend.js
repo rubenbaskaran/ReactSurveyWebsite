2a123c37b401890fcc7ca733530e537ab4fcf00c
Ruben Baskaran <rbaskaran92@gmail.com>
2021-02-08 22:56:10 +0100

    seperated into two instruments

--- src/components/CallsToBackend.js
+++ src/components/CallsToBackend.js
@@ -66,9 +66,7 @@ function UploadFirst1To7(input, dispatch) {
     question_55: "",
     question_56: "",
     question_57: "",
-    question_58: "",
     answers_complete: 0,
-    emails_complete: 0,
   };
 
   console.log("UploadFirst1To7 - REQUEST:");
@@ -142,9 +140,7 @@ function UploadSecond8To26(input) {
     question_55: "",
     question_56: "",
     question_57: "",
-    question_58: "",
     answers_complete: 0,
-    emails_complete: 0,
   };
 
   console.log("UploadSecond8To26 - REQUEST:");
@@ -218,9 +214,7 @@ function UploadThird27To34(input) {
     question_55: "",
     question_56: "",
     question_57: "",
-    question_58: "",
     answers_complete: 0,
-    emails_complete: 0,
   };
 
   console.log("UploadThird27To34 - REQUEST:");
@@ -294,9 +288,7 @@ function UploadVideoId(input) {
     question_55: "",
     question_56: "",
     question_57: "",
-    question_58: "",
     answers_complete: 0,
-    emails_complete: 0,
   };
 
   console.log("UploadVideoId - REQUEST:");
@@ -370,9 +362,7 @@ function UploadFourth35To49(input) {
     question_55: "",
     question_56: "",
     question_57: "",
-    question_58: "",
     answers_complete: input.answers_complete,
-    emails_complete: 0,
   };
 
   console.log("UploadFourth35To49 - REQUEST:");
@@ -446,9 +436,7 @@ function UploadFifth50To57(input) {
     question_55: input.question_55,
     question_56: input.question_56,
     question_57: input.question_57,
-    question_58: "",
     answers_complete: 2,
-    emails_complete: 0,
   };
 
   console.log("UploadFifth50To57 - REQUEST:");
@@ -461,69 +449,7 @@ function UploadSixth58(input) {
   let url = "https://respekt-backend.herokuapp.com/update";
   let data = {
     record_id: "null",
-    video_id: "",
-    timestamp_start: "",
-    timestamp_end: "",
-    survey_duration: "",
-    question_1: "",
-    question_2: "",
-    question_3: "",
-    question_4: "",
-    question_5: "",
-    question_6: "",
-    question_7: "",
-    question_8: "",
-    question_9: "",
-    question_10: "",
-    question_11: "",
-    question_12: "",
-    question_13: "",
-    question_14: "",
-    question_15: "",
-    question_16: "",
-    question_17: "",
-    question_18: "",
-    question_19: "",
-    question_20: "",
-    question_21: "",
-    question_22: "",
-    question_23: "",
-    question_24: "",
-    question_25: "",
-    question_26: "",
-    question_27: "",
-    question_28: "",
-    question_29: "",
-    question_30: "",
-    question_31: "",
-    question_32: "",
-    question_33: "",
-    question_34: "",
-    question_35: "",
-    question_36: "",
-    question_37: "",
-    question_38: "",
-    question_39: "",
-    question_40: "",
-    question_41: "",
-    question_42: "",
-    question_43: "",
-    question_44: "",
-    question_45: "",
-    question_46: "",
-    question_47: "",
-    question_48: "",
-    question_49: "",
-    question_50: "",
-    question_51: "",
-    question_52: "",
-    question_53: "",
-    question_54: "",
-    question_55: "",
-    question_56: "",
-    question_57: "",
     question_58: input.question_58,
-    answers_complete: "",
     emails_complete: 2,
   };
 
