9aa5ad9089f9c1100d51309c8fefbc3886d3c189
Ruben Baskaran <rbaskaran92@gmail.com>
2021-02-27 12:02:14 +0100

    scan page 5

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import store from "./globalVariables";
 import { Provider } from "react-redux";
 
 const App = () => {
-  const [currentPage, setCurrentPage] = React.useState(4);
+  const [currentPage, setCurrentPage] = React.useState(5);
 
   return (
     <Provider store={store}>
