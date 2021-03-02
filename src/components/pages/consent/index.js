987c0a7da30b92d5f2b5868a6bf62b4500e2bc98
Ruben Baskaran <rbaskaran92@gmail.com>
2021-03-02 16:07:13 +0100

    changed marginleft on link

--- src/components/pages/consent/index.js
+++ src/components/pages/consent/index.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles({
   link: {
     color: "#0029E8",
     "padding-bottom": "2.5px",
-    "margin-left": "-1px",
+    "margin-left": "-0.5px",
     fontSize: "15px",
     "font-style": "italic",
   },
